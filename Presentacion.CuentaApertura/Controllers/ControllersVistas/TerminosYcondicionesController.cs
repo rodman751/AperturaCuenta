47c6a9bf1b3a993913dd468f182fb761a5ac9f8b
RELIC7663 <alexispepinosa28@gmail.com>
2024-08-19 22:47:39 -0500

    TerminosYcondiciones view

---
+++ Presentacion.CuentaApertura/Controllers/ControllersVistas/TerminosYcondicionesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentacion.CuentaApertura.Controllers.ControllersVistas
+{
+    public class TerminosYcondicionesController : Controller
+    {
+        // GET: TerminosYcondicionesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
